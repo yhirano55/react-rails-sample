1b6d01b1103957eedbc6af4614e3495055bd3f5a
Yoshiyuki Hirano <yhirano@aiming-inc.com>
2016-12-23 14:38:12 +0900

    modify CommentBox

--- app/assets/javascripts/components/comment_box.js.jsx
+++ app/assets/javascripts/components/comment_box.js.jsx
@@ -1,6 +1,10 @@
 var CommentBox = React.createClass({
 
   render: function() {
-    return <div />;
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox
+      </div>
+    );
   }
 });
